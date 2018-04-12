68e46a7eb125b0a9ae7aa3517c7ad21ca0c4bbfa
Cole Hansen <cole.hansen@gmail.com>
2018-04-11 19:01:15 -0700

    Write messages to object in Firebase with name of localKey and pull the messages only from there

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ $(document).ready(function () {
 
     var messageKey = '';
     var messageText = '';
+    //this is used to determine the session for each chat and only pull from that session
+    var localKey = 54321;
+
+    localStorage.setItem("localKey", localKey);
 
     var config = {
         apiKey: "AIzaSyA5M-HShYfmi1BIVC7_zZlzLp8G9ttmQJw",
@@ -23,32 +27,30 @@ $(document).ready(function () {
     $("#send-message").on("click", function () {
         messageText = $('#message').val().trim();
         messageKey = 123;
-        console.log(messageText);
-        database.ref('messages').push({
+        // console.log(messageText);
+        //This will create a new object in the Firebase with name of localKey
+        database.ref(localKey).push({
             message: messageText,
             key: messageKey,
             date: Date.now()
         });
     });
 
-    database.ref('messages').on("child_added", function (snapshot) {
+    // this will only pull messages from the object in Firebase with this session's localKey
+    database.ref(localKey).on("child_added", function (snapshot) {
         var newDiv = $("<div>");
         var msgText = snapshot.val().message;
         var msgDate = moment(snapshot.val().date).format("HH:mm");
+        // localKey = snapshot.key;
 
         console.log(msgText);
         console.log(msgDate);
+        console.log(localKey);
 
         var newMessage = newDiv.text(msgText + " - " + msgDate);
 
         $("#chat-box").append(newMessage);
     });
-
-    function writeMessage(message) {
-        var newDiv = $("<div>");
-
-        $("#message").prepend(newMessage)
-    }
 
 
 });
